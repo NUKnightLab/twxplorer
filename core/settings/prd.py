a810dc6d00b8612ecb182e1c014aa422881b1440
jenny <jennifer.y.wilson@northwestern.edu>
2013-09-12 11:13:29 -0500

    add production settings

--- core/settings/prd.py
+++ core/settings/prd.py
@@ -5,9 +5,17 @@ from .base import *
 
 # Import secrets
 sys.path.append(
-    os.path.normpath(os.path.join(PROJECT_ROOT, '../secrets/twxplorer/stg'))
+    os.path.normpath(os.path.join(PROJECT_ROOT, '../secrets/twxplorer/prd'))
 )
 from secrets import *
 
 # Set static URL
-STATIC_URL = 'http://media.knightlab.us/twxplorer/'
+STATIC_URL = 'http://media.knightlab.com/twxplorer/'
+
+# Database
+MONGODB_HOST = 'prd-mongo1.knilab.com'
+MONGODB_PORT = 27017
+MONGODB_NAME = 'twxplorer'
+
+# Flask configuration
+os.environ['FLASK_CONFIG_MODULE'] = os.path.join(secrets_path, 'flask_config.py')
